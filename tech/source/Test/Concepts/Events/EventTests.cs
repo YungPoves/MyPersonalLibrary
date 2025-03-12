8585128270a0879ccdcf32d0ef9b6f5e4f8fe466
YungPoves <povesr12@gmail.com>
2025-03-12 14:33:06 +0200

    Test/Concepts - Add EventTests.

---
+++ tech/source/Test/Concepts/Events/EventTests.cs
@@ -0,0 +1,17 @@
+namespace Test.Concepts
+{
+  [TestClass]
+  public class EventTests
+  {
+    [TestInitialize]
+    public void Setup()
+    {
+    }
+
+    [TestMethod]
+    public void Test1()
+    {
+      Assert.IsTrue(1 == 1);
+    }
+  }
+}
