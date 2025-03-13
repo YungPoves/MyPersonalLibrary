78ec218ef6553ff461a1dc404a8b2ff0cf673fe7
YungPoves <povesr12@gmail.com>
2025-03-13 08:46:05 +0200

    Test/Concepts - Add EventAndDelegateTests.

---
+++ tech/source/Test/Concepts/EventsAndDelegates/EventAndDelegate.cs
@@ -0,0 +1,24 @@
+namespace Test.Concepts.EventsAndDelegates
+{
+  [TestClass]
+    public class EventAndDelegate
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        [TestMethod]
+        public void TestDelegates()
+        {
+        }
+        [TestMethod]
+        public void TestEvents()
+        {          
+        }
+        [TestMethod]
+        public void TestEventsVsDelegates()
+        {          
+        }
+    }
+}
