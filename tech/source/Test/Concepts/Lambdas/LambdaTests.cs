b1735e1f3d1bc843d0212b8aaefc4071fcb051be
YungPoves <povesr12@gmail.com>
2025-04-16 16:23:57 +0200

    LambdaTests - TestLambdaAction

--- tech/source/Test/Concepts/Lambdas/LambdaTests.cs
+++ tech/source/Test/Concepts/Lambdas/LambdaTests.cs
@@ -18,7 +18,22 @@ namespace Test.Concepts.Lambdas
     {
       hello("Steve");
     }
+    [TestMethod]
+    public void TestLambdaExpression()
+    {
+      IEnumerable<int> squaredNumbers = numbers.Select(x => x * x);
+      Console.WriteLine(string.Join(" ", squaredNumbers));
+    }
+    [TestMethod]
+    public void TestLambdaAction()
+    {
+      IntFunc = (x, y) => x * y;
+      int multipliedInts = IntFunc(5, 4);
+      Assert.IsTrue(multipliedInts == 20);
+    }
 
+    Func<int, int, int>? IntFunc;
+    readonly int[] numbers = [2, 3, 4, 5];
     readonly Func<int, int> sqaured = (x) => x * x;
     readonly Action<string> hello = (name) => Console.WriteLine($"Hello {name}!");
   }
